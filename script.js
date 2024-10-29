dd8b5fa8ec01f880af192d83011d5741c9c15b9b
n2olivier <139780882+n2olivier@users.noreply.github.com>
2024-10-30 00:23:12 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementById('signUpOverlay');
+const signInButton = document.getElementById('signInOverlay');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
