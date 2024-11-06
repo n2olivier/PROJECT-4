5c6feb52a1c7182a916bc18a2f1d1b833b3d614d
n2olivier <139780882+n2olivier@users.noreply.github.com>
2024-11-06 15:19:00 +0200

    Initial commit

---
+++ order.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LAVENDER KF102 drone',
+        productNumber: '36378',
+        paymentStatus: 'refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with camera',
+        productNumber: '96996',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'DJI 4k Drone',
+        productNumber: '2282',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
